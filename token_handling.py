2ab704069b08c554c6e3356801c4f70c98fc4e29
daniboomberger <daniboomberger@protonmail.com>
2021-01-25 20:22:58 +0100

    created initialise function for the discord client

--- token_handling.py
+++ token_handling.py
@@ -6,9 +6,14 @@ import discord
 class Discord():
     def __init__(self):
         self.token = ''
+        self.client = ''
     
     #Saves Discord Token in a Json File
     def saveDiscordToken(self):
         token_file = open('credentials.json', 'w')
         token_json = {"TOKEN": self.token}
-        token_file.write(json.dumps(token_json))        
+        token_file.write(json.dumps(token_json))
+
+    def initialiseDiscordCLient(self):
+        self.client = discord.Client()
+        self.client.run(self.token)
