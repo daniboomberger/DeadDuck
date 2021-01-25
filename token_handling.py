8d35870f6410b313f5c0b1777ec84b062f65bbce
daniboomberger <daniboomberger@protonmail.com>
2021-01-25 20:24:06 +0100

    changed format of comment and added new function  comment

--- token_handling.py
+++ token_handling.py
@@ -8,12 +8,13 @@ class Discord():
         self.token = ''
         self.client = ''
     
-    #Saves Discord Token in a Json File
+    # Saves Discord Token in a Json File
     def saveDiscordToken(self):
         token_file = open('credentials.json', 'w')
         token_json = {"TOKEN": self.token}
         token_file.write(json.dumps(token_json))
 
+    # Initialize Discor client to make bot available 
     def initialiseDiscordCLient(self):
         self.client = discord.Client()
         self.client.run(self.token)
